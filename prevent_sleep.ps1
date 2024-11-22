3a5525cf5cbf670b3b8d3f9c38debecede123409
HoodedUnicorn <68126304+HoodedUnicorn@users.noreply.github.com>
2024-11-22 19:00:59 +0100

    File has been deprecated

--- prevent_sleep.ps1
+++
@@ -1,5 +0,0 @@
-while ($true) {
-    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(1,1)
-    Start-Sleep -Seconds 60
-    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(2,2)
-}
