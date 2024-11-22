83c740b5ba8ba8c759b756a74593adb3a1dc82e3
HoodedUnicorn <68126304+HoodedUnicorn@users.noreply.github.com>
2024-11-22 16:32:28 +0100

    Corrected run_prevent_sleep.bat
    Changed & for ;

--- run_prevent_sleep.bat
+++ run_prevent_sleep.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Disable sleep settings
-powershell -Command "powercfg /change standby-timeout-ac 0 & powercfg /change standby-timeout-dc 0 & powercfg /change monitor-timeout-ac 0 & powercfg /change monitor-timeout-dc 0"
+powershell -Command "powercfg /change standby-timeout-ac 0; powercfg /change standby-timeout-dc 0; powercfg /change monitor-timeout-ac 0; powercfg /change monitor-timeout-dc 0"
 
 :: Run PowerShell prevent sleep script
 powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Scripts\prevent_sleep.ps1"
